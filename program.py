b107466710128bbdec212cf70f6c9a6f2db114cc
Kamil Gawlik <kamilmgawlik@gmail.com>
2016-07-02 16:07:31 +0200

    Initial commit

---
+++ program.py
@@ -0,0 +1,23 @@
+adding = True
+stringtable = []
+while adding:
+    x = input()
+    if x == " ": adding = False
+    stringtable.append(x)
+    print(stringtable)
+'''s1 = input()
+s2 = input()
+
+table1 = list(s1)
+table2 = list(s2)'''
+'''
+print(table1, table2)
+
+if len(s1) >len(s2):
+    lenght = len(s1)
+else:
+    lenght = len(s2)
+
+for x in range(0,lenght):
+    print(table1[x], table2[x])'''
+
